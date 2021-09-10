1d4b3aabc716fffc70b2b56d3d9ff96499840723
Igor Sasaki <igor@softable.com.br>
2021-09-10 20:28:27 -0300

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+// Bibliotecas Externas
+import React from 'react';
+
+// Componentes
+import {Home} from './src/pages/Home';
+
+export function App() {
+  return <Home />;
+}
+
+export default App;
