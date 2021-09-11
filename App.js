7528bacc17fbfe895eef373da7e5cb12732d2517
Igor Sasaki <igor-sasaki@hotmail.com>
2021-09-10 21:33:36 -0300

    style(home): estiliza e personaliza mensagem de acordo com a hora do dia, estiliza statusBar

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 // Bibliotecas Externas
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 // Componentes
 import {Home} from './src/pages/Home';
 
 export function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <Home />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 // Bibliotecas Externas
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Platform} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
 
 // Componentes
 import {Button} from '../components/Button';
@@ -9,6 +16,19 @@ import {SkillCard} from '../components/SkillCard';
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good Afternoon');
+    } else {
+      setGreeting('Good Night');
+    }
+  }, []);
 
   const handleAddNewSkill = () => {
     setMySkills([...mySkills, newSkill]);
@@ -18,6 +38,8 @@ export function Home() {
   return (
     <View style={styled.container}>
       <Text style={styled.title}>Welcome, Igor Sasaki</Text>
+
+      <Text style={styled.greeting}>{greeting}</Text>
 
       <TextInput
         style={styled.input}
@@ -31,9 +53,11 @@ export function Home() {
 
       <Text style={[styled.title, {marginVertical: 50}]}>My Skills</Text>
 
-      {mySkills.map(skill => (
-        <SkillCard skill={skill} key={`${skill}_${Math.random()}`} />
-      ))}
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => `${item}_${Math.random()}`}
+        renderItem={({item}) => <SkillCard skill={item} />}
+      />
     </View>
   );
 }
@@ -57,5 +81,8 @@ const styled = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greeting: {
+    color: '#fff',
   },
 });
